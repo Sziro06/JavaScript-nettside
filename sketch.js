21c971347b05f960a1cc1da0c089b6aaa8588f1d
Sziro06 <huoea071@gmail.com>
2023-01-16 10:48:03 +0100

    oppdateringer

--- sketch.js
+++ sketch.js
@@ -1,8 +1,8 @@
 const menu = document.querySelector(".menu");
 const menuItems = document.querySelectorAll(".menuItem");
 const hamburger= document.querySelector(".hamburger");
-const closeIcon= document.querySelector(".closeIcon");
-const menuIcon = document.querySelector(".menuIcon");
+const lukkeIkonen = document.querySelector(".lukkeIkonen");
+const menuIkonen = document.querySelector(".menuIkonen");
 
 function toggleMenu() {
   if (menu.classList.contains("showMenu")) {
